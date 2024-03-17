8669783282f071b11510a249eda64aef0eee64a1
Miguel21S <leaismiguelss@gamail.com>
2024-03-17 16:35:12 +0100

    Update and Feat: creation of a like model

--- entities/likes/LikesModel.ts
+++ entities/likes/LikesModel.ts
@@ -1,8 +1,46 @@
-import { Document, Schema } from "mongoose"
+import { Document, Schema, model } from "mongoose"
 
-
-interface likes extends Document {
+interface Likes extends Document {
+    idPost: Schema.Types.ObjectId;
     userIdPost: Schema.Types.ObjectId;
-    userName: string;
-    
+    userIdLike: Schema.Types.ObjectId;
+    titlePost: string;
+    userNamePost: string;
+    userNameLike: string;
+    lik: number;
 }
+
+const likesSchema = new Schema<Likes>(
+    {
+        idPost:
+        {
+            type: Schema.Types.ObjectId,
+            ref: "PostModel"
+        },
+
+        userIdPost:
+        {
+            type: Schema.Types.ObjectId,
+            ref: "PostModel"
+        },
+
+        userIdLike:
+        {
+            type: Schema.Types.ObjectId,
+            ref: "UserModel"
+        },
+
+        titlePost: String,
+        userNamePost: String,
+        userNameLike: String,
+        lik: Number,
+    },
+
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+)
+
+const LikeModel = model<Likes>("Likes", likesSchema);
+export default LikeModel;
