15d4180c741721fe45e75442124e0778546cb2ab
Miguel21S <leaismiguelss@gamail.com>
2024-03-19 23:49:17 +0100

    Update: routers

--- src/entities/controllers/routes.ts
+++ src/entities/controllers/routes.ts
@@ -1,13 +1,10 @@
 
 import * as controller from "./authControllers";
-import { Request, Response, Router } from "express";
+import { Router } from "express";
 
 const router = Router();
 
 router.post('/auth/register', controller.registrar);
 router.post('/auth/login', controller.login);
-router.get("/api/healthy", (req: Request, res: Response) => {
-    res.status(200).json({ success: true, message: "server is healthy" })
-  })
 
 export default router;
